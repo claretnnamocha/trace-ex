9375041e8d9ba3ad99a3dad91c80db2560c1f83c
claretnnamocha <devclareo@gmail.com>
2022-11-19 21:52:00 +0100

    Update App.ts

--- src/models/App.ts
+++ src/models/App.ts
@@ -82,6 +82,8 @@ const App = db.define(
 
         for (let index = 0; index < tokens.length; index += 1) {
           const { blockchain, network, symbol } = tokens[index];
+          console.log({ blockchain, network, symbol });
+
           await generateWallet({
             appId,
             blockchain,
